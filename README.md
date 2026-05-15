6eb0932742e24d5d0b539984f956788a53c4ac81
João Pedro <joaopedro.soareslima07@gmail.com>
2026-05-14 22:34:38 -0300

    Estruturas de repetiçoes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BootCamp_santatnder
 Meus estudos para o bootcamp santander
+
+<h1>Meu instagram: @jottape.s2x</h1>
---
+++ laco_Rep_estrutura_candicional/estrutura_Condicionais.py
@@ -0,0 +1,23 @@
+#FOR --- usaremos quando sabemos a quantidade de vezes que o código irá se repetir 
+#Exepmlo
+for i in range(0 , 10):
+    
+    print(i, end= ' ')
+    
+letra = 'Joao'
+
+print()
+
+for l in letra:
+    
+    print(l, end= ' ')
+    
+print()
+
+#While -- Usado quando quer repetir o laço até que uma condição seja verdadeira
+
+var = 10
+i = 0
+while i < var:
+    print(i, end=" ")
+    i +=1

563e495286615b6419f58ea64adc0a7637e77818
João Pedro <joaopedro.soareslima07@gmail.com>
2026-05-17 19:25:54 -0300

    Manipulando strings

---
+++ manipulando_strings/metodos.py
@@ -0,0 +1,17 @@
+nome = 'joÃo'
+print(nome)
+#método  upper() todas as letras ficam  em maiusculo
+print(nome.upper())
+#Método lower()  todas as letras ficam em minusculo
+print(nome.lower())
+#métedo title()  Deixa a primeira letra maiscula 
+print(nome.title())
+
+
+nome = '  joÃo   p'
+#metodo strip() usado para remover espaços em brancos dentro de uma string 
+print(nome.strip())
+#metodo lstrip() remove só o spaço da esquerda
+print(nome.lstrip())
+#metodo rstrip() remove o espaço da direita 
+print(nome.rstrip())

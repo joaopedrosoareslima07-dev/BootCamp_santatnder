ab16c1c870ecf53defdc88197513d9ec5f027f19
João Pedro <joaopedro.soareslima07@gmail.com>
2026-06-05 09:37:49 -0300

    Agora deu certo

--- pprojetos/banco_2-3/banco.py
+++ pprojetos/banco_2-3/banco.py
@@ -35,26 +35,113 @@ def criar_usuario(usuarios):
     print('Cadastro realizado com sucesso! ')
     
     
-    def criar_contas(agencia, numero_conta, usuarios):
+def criar_contas(agencia, numero_conta, usuarios):
         pedidoCpf = input('Informe seu CPF: ')
         for verficacao in usuarios:
             if pedidoCpf == verficacao['Cpf']:
                 
                 
                 return  {
-                    'Agência' : '0001',
+                    'Agência' : agencia,
                     'Numero conta ': numero_conta,
                     'Usuário': verficacao
                 }
                 
-        else:
-            print('Pessoa não cadastrada ')
 
-
-
+        print('Pessoa não cadastrada ')
+        return None
+    
+def soma(saldo, valor_deposito,extrato,/):
+    saldo += valor_deposito
+    print('Valor depositado com sucesso! ')
+    print(f'Valor atual {saldo:.2f}')
+    extrato.append(f' Depósito R$: {valor_deposito}')
+    return saldo, extrato
+    
+def saque(*,saldo, valor_saque, extrato, limite_saque = 500, totalsaque = 0, maximoSaque = 3):
+    excedeu_limite_diario = totalsaque >= maximoSaque
+    excedeu_limite = valor_saque > limite_saque
+    if excedeu_limite_diario:
+        print('Limite de saque diário atingido ')
+        return saldo, extrato, totalsaque
+    elif  excedeu_limite:
+        print('Valor do saque maior que o limite permitido ')
+        return saldo, extrato, totalsaque
+    elif valor_saque < saldo and valor_saque > 0:
+        saldo -= valor_saque
+        print('Valor sacado com sucesso! ')
+        print(f'Valor atual {saldo:.2f}')
+        totalsaque += 1
+        extrato.append(f' Saque R$: {valor_saque}')
+        return saldo, extrato, totalsaque
     
     
+def verSaldo(*,saldo):
+    print(f'Você tem um saldo de R$: {saldo}')
     
+    
+def verExtrato(extrato):
+    if not extrato:
+        print('Não foram realizadas movimentações no banco!')
+        
+    else:
+        for movi in extrato:
+            print(movi)
+        
+extrato = []
+saldo = 1000
+totalSaque = 0
+
+while True:
+        print('''--------------------------------------
+                       Bem-Vindo ao banco
+                       Escolha sua operação!
+                       [01] Criar usuário
+                       [02] Criar conta 
+                       [03] Depositar 
+                       [04] Sacar
+                       [05] Ver Saldo 
+                       [06] Ver extrato
+                       [07] Sair 
+                  --------------------------------------
+''')
+        opcao = int(input('Escolha sua opção: '))
+
+        match opcao:
+            case 1:
+                criar_usuario(usuarios)
+
+            case 2:    
+                numeroConta = len(contas) + 1
+                nova_conta = criar_contas(agencia= '0001', numero_conta= numeroConta, usuarios= usuarios)
+                
+                if nova_conta :
+                    contas.append(nova_conta)
+                    
+            case 3:
+                valorDep = float(input('Informe o valor que você deseja depositar '))
+                saldo, extrato = soma(saldo, valorDep, extrato)
+                
+            case 4:
+                valorSaque = float(input('Informe o valor que você deseja sacar!: '))
+                
+                saldo,extrato, totalSaque = saque(saldo= saldo, valor_saque=valorSaque, extrato=extrato, totalsaque= totalSaque)
+                
+            case 5:
+                verSaldo(saldo= saldo)
+                
+            case 6:
+                
+                verExtrato(extrato)
+                
+            case 7:
+                print('Saindo...')
+                break
+            
+            case _:
+                print('Opção inválida!')
+                
+            
     
     
     
